a1f86dcd47388a72e64aa326b9731083e64020f9
Michael Galloy <mgalloy@gmail.com>
2016-01-28 15:41:40 -0700

    More Python bindings updates

---
+++ Python/hexic_example.py
@@ -0,0 +1,4 @@
+import hexic
+
+if __name__ == '__main__':
+    print hexic.invert([])
--- Python/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    ...
-    setup_requires=['cffi>=1.0.0'],
-    cffi_modules=["build_bindings.py:ffi"],
-    install_requires=["cffi>=1.0.0"],
-)
