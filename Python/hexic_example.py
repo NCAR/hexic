432ced6813bd2724916e68141822e5a5092a10e2
Michael Galloy <mgalloy@gmail.com>
2016-01-28 15:57:26 -0700

    Better Python example

--- Python/hexic_example.py
+++ Python/hexic_example.py
@@ -1,4 +1,20 @@
+import numpy
+
 import hexic
 
+
+def main():
+    # TODO: read some example FITS file
+    nx = 2
+    ny = 2
+    n_filters = 6
+    n_pol_states = 4
+    observations = numpy.arange(0., n_filters * n_pol_states * nx * ny, 1.0, dtype='f')
+    observations = observations.reshape((n_filters, n_pol_states, nx, ny))
+
+    # compute the inversion
+    result = hexic.invert(observations)
+
+
 if __name__ == '__main__':
-    print hexic.invert([])
+    main()
