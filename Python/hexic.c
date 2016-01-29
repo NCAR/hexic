179b55c983efe77ec5ae0fdce079add7e6579286
Michael Galloy <mgalloy@gmail.com>
2016-01-28 19:11:15 -0700

    Calling from Python to Fortran

--- Python/hexic.c
+++ Python/hexic.c
@@ -10,20 +10,32 @@
 static PyObject *py_hexic_invert(PyObject *self, PyObject *args) {
   PyArrayObject *observations_array = NULL;
   double *observations;
+  npy_intp *shape;
+  PyArray_Descr *dtype;
+  int status;
 
   if (!PyArg_ParseTuple(args, "O!", &PyArray_Type, &observations_array)) return NULL;
 
   // TODO: check observations array is:
   //   - type double
-  //   - 4-dimensional
-  //   - 6 filters
-  //   - 4 polarization states
 
+  dtype = PyArray_DTYPE(observations_array);
+
+  if (PyArray_NDIM(observations_array) != 4) {
+    printf("wrong number of dimensions\n");
+  }
+  shape = PyArray_SHAPE(observations_array);
+  if (shape[0] != 6) {
+    printf("wrong number of filters\n");
+  }
+  if (shape[1] != 4) {
+    printf("wrong number of polarization states\n");
+  }
   observations = (double *) PyArray_DATA(observations_array);
 
-  // TODO: call hexic_invert
+  status = hexic_invert(observations, shape[3], shape[4], shape[0]);
 
-  return Py_BuildValue("i", 0);
+  return Py_BuildValue("i", status);
 }
 
 static PyMethodDef hexic_methods[] = {
