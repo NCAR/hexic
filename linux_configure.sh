a6dbf30cc2dd68664078761e54e6f6ab1bedaa9d
Michael Galloy <mgalloy@gmail.com>
2016-10-31 14:34:55 -0600

    Working build on Linux

--- linux_configure.sh
+++ linux_configure.sh
@@ -7,4 +7,6 @@ cd build
 cmake \
   -DCMAKE_INSTALL_PREFIX:PATH=~/software/hexic \
   -DIDL_ROOT_DIR:PATH=/opt/share/idl8.5/idl85 \
+  -DCMAKE_LIBRARY_PATH:PATH=~/software/lib64 \
+  -DCMAKE_INCLUDE_PATH:PATH=~/software/include \
   ..
